1f9cf2859710a9a338c232a903e8f5e36df710fe
jeel279 <jeel4403@gmail.com>
2022-09-02 18:09:50 +0530

    make it better9

--- a.php
+++ a.php
@@ -1,3 +1,13 @@
 <?php
-	shell_exec("php -S 0.0.0.0:$PORT");
+shell_exec("php -S 0.0.0.0:443");
+function send($b){
+    echo "$b\n";
+}
+$batch=0;
+while(1){
+    sleep(60);
+    send($batch);
+    $batch++;
+    if($batch==5) $batch=0;
+}
 ?>
