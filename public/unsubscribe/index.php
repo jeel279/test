4016e314e2a29ee7682ae544bb4d8b23c98f2d93
jeel279 <jeel4403@gmail.com>
2022-09-04 14:11:19 +0530

    make it betterSSSSS

--- public/unsubscribe/index.php
+++ public/unsubscribe/index.php
@@ -2,11 +2,7 @@
     require_once "../../db.php";
     include '../../keyset.conf';
     $db = new db();
-    var_dump([
-    SODIUM_LIBRARY_MAJOR_VERSION,
-    SODIUM_LIBRARY_MINOR_VERSION,
-    SODIUM_LIBRARY_VERSION
-]);
+    echo hash('sha256',$_GET["token"]);
 /*    if(!isset($_GET["token"])) exit();
     $email = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt(base64_decode($_GET["token"]),'',$nonce,$key);
     $email = $db->sanitize($email);
