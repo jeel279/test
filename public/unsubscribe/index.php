77d7e09c706b882d522384a82d039c55d2de80b3
jeel279 <jeel4403@gmail.com>
2022-09-04 14:08:34 +0530

    make it betterSSSSS

--- public/unsubscribe/index.php
+++ public/unsubscribe/index.php
@@ -2,7 +2,12 @@
     require_once "../../db.php";
     include '../../keyset.conf';
     $db = new db();
-    if(!isset($_GET["token"])) exit();
+    var_dump([
+    SODIUM_LIBRARY_MAJOR_VERSION,
+    SODIUM_LIBRARY_MINOR_VERSION,
+    SODIUM_LIBRARY_VERSION
+]);
+/*    if(!isset($_GET["token"])) exit();
     $email = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt(base64_decode($_GET["token"]),'',$nonce,$key);
     $email = $db->sanitize($email);
     $db->query("DELETE FROM mailing_list WHERE email='".$email."'");
@@ -10,6 +15,6 @@
         echo "Unsubscribed";
     }else{
         header("Location : .");
-    }
+    }*/
 
 ?>
