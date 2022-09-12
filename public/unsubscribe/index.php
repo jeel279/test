bf7903db3c1d2f422864480182422c08184f4ea6
jeel279 <jeel4403@gmail.com>
2022-09-12 10:57:18 +0530

    make it better25755885848

--- public/unsubscribe/index.php
+++ public/unsubscribe/index.php
@@ -1,14 +1,16 @@
 <?php
     require_once "../../db.php";
-    include '../../keyset.conf';
     $db = new db();
     if(!isset($_GET["token"])) exit();
-    $token = $db->sanitize($token);
-    $db->query("DELETE FROM mailing_list WHERE identifier='".$token."'");
-    if($res){
+    $token = $db->sanitize($_GET["token"]);
+    $resA = $db->query("SELECT * FROM mailing_list WHERE identifier='".$token."'");
+    if((int)$resA->num_rows>0){
+    $res = $db->query("DELETE FROM mailing_list WHERE identifier='".$token."'");
+     if($res){
         echo "Unsubscribed";
-    }else{
-        header("Location : .");
-    }
+     }
+}else{
+        echo "Bad Token : $token";
+}
 
 ?>
