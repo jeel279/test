f06fe335f012befbf95248374d09572a9739734a
jeel279 <jeel4403@gmail.com>
2022-09-15 08:08:18 +0530

    make it betterv5

--- public/unsubscribe/index.php
+++ public/unsubscribe/index.php
@@ -2,8 +2,7 @@
     require_once "../../db.php";
     $db = new db();
     if(!isset($_GET["token"])) exit();
-    
-    $token = $db->sanitize($token);
+    $token = $db->sanitize($_GET["token"]);
     $resA = $db->query("SELECT * FROM mailing_list WHERE identifier='".$token."'");
     if((int)$resA->num_rows>0){
     $res = $db->query("DELETE FROM mailing_list WHERE identifier='".$token."'");
